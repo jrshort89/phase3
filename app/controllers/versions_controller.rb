6dffd0b5cae83321e5a6b9214c70837933555e15
raaynaldo <raaynaldo@hotmail.com>
2020-12-01 11:17:01 -0500

    update routes

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,2 +1,8 @@
 class VersionsController < ApplicationController
+    def update_bulk
+        versionIds = params["versionIds"]
+        stage = params["stage"]
+        versions = Version.where(id: params["versionIds"]).update_all(stage: stage)
+        render json: versions
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get '/repositories/:id/documents', to: 'repositories#getDocuments'
+  patch '/versions/bulk', to: 'versions#update_bulk'
+
   resources :versions
   resources :documents
   resources :commits
   resources :repositories
 
-  get '/repositories/:id/documents', to: 'repositories#getDocuments'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
